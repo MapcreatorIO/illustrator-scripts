81836fef58e1766af473e1210ea3194727d56274
doskabouter <paul.doskabouter@gmail.com>
2023-06-14 17:06:27 +0200

    Removed commented out code Added comments about the working

--- fixtextonpath.js
+++ fixtextonpath.js
@@ -1,4 +1,10 @@
-// (c) 2022 MapCreator BV, the Netherlands
+// (c) 2023 MapCreator BV, the Netherlands
+// This script transforms all texts that were incorrectly imported from SVG into Illustrator
+// This only works for SVG files coming out of mapcreator.io, because a tag 'fixtextpath' and the original geometry of that path is added to all text-items that are going to be imported incorrectly
+//
+// For each such a text-item, all textframe contents are gathered (in textFrameContents) as well as the geometry (in pathItem)
+// from that, a new text-item is created as a text-on-path item
+
 {
     var items = app.activeDocument.groupItems;
     for (var i = items.length - 1; i >= 0; i--) {
@@ -14,18 +20,14 @@ function FixPath(item) {
     var templateFrames = [];
     for (var i = 0; i < item.pageItems.length; i++) {
         var subItem = item.pageItems[i];
-//alert('sub '+subItem.typename+' '+i);
         if (subItem.typename == 'TextFrame') {
             textFrameContents = subItem.contents;
         } else {
             if (subItem.typename == 'GroupItem') {
                 var total = '';
                 for (var j = 0; j < subItem.textFrames.length; j++) {
-    //alert('group '+i+' subitem '+j);
-                        //alert('tf contents:'+templateFrame.contents);
                     total = subItem.textFrames[j].contents + total;
                 }
-                //alert(total);
                 if (subItem.textFrames.length > 0) {
                   subItem.textFrames[0].contents = total + textFrameContents;
                   var p=subItem.textFrames[0].paragraphs[0].paragraphAttributes;
@@ -39,8 +41,6 @@ function FixPath(item) {
     }
     if (pathItem != null && item.parent.typename == 'GroupItem' && templateFrames.length > 0) {
         for (var i = templateFrames.length-1; i>=0; i--) {
-            //templateFrame.contents = total;
-            //var nw = item.parent.textFrames.pathText(pathItem, 0, 0, TextOrientation.HORIZONTAL, templateFrames[i]);
             var nw = item.parent.textFrames.pathText(pathItem.duplicate(), 0, 0, TextOrientation.HORIZONTAL, templateFrames[i]);
         }
         item.remove();
